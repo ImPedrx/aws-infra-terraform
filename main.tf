6dbec8b8fa0eb7bab82df1303f9d45c9eb383fe0
MARTINS DE PAULA RIBEIRO <pedrohenrique.martinsdepaula@aernnova.com>
2026-05-18 10:41:17 -0300

    fix line 12 main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,8 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "pedrx-remotestate-bucket"
-    key    = "aws-Staes/terraform.tfstate"
+    bucket = "pedrx-terraform-state-2025"
+    key    = "aws-states/terraform.tfstate"
     region = "us-east-2"
   }
 }
